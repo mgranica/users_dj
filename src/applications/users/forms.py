33aadf1f716adec9218a103081b13c95b2c3f233
mgranica <migue.granica@gmail.com>
2021-03-07 20:21:57 +0100

    create new user - form view

--- src/applications/users/forms.py
+++ src/applications/users/forms.py
@@ -5,8 +5,34 @@ from .models import User
 class UserRegisterForm(forms.ModelForm):
     """Form definition for UserRegister."""
 
+    password1 = forms.CharField(
+        label='Contraseña',
+        required=True,
+        widget=forms.PasswordInput(
+            attrs={
+                'placeholder': 'Contraseña'
+            }
+        )
+    )
+
+    password2 = forms.CharField(
+        label='Contraseña',
+        required=True,
+        widget=forms.PasswordInput(
+            attrs={
+                'placeholder': 'Repetir Contraseña'
+            }
+        )
+    )
+    
     class Meta:
         """Meta definition for UserRegisterform."""
 
         model = User
-        fields = ('__all__')
+        fields = (
+            'username',
+            'email',
+            'name',
+            'last_names',
+            'gender'
+        )
--- src/applications/users/views.py
+++ src/applications/users/views.py
@@ -4,11 +4,16 @@ from django.views.generic import (
     CreateView
 )
 
+from django.views.generic.edit import (
+    FormView
+)
+
+
 from .forms import UserRegisterForm
 
 # Create your views here.
 
-class UserRegisterView(CreateView):
+class UserRegisterView(FormView):
     template_name = 'users/register.html'
     form_class = UserRegisterForm
     success_url = '/'
